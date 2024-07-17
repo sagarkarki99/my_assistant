05cfd58cfb7357bb8a19796b115d7e5c84a76015
sagarkarki99 <sagarkarki184@gmail.com>
2024-07-17 15:17:07 +0400

    refactor: update exception object

--- lib/cubit/chat_cubit.dart
+++ lib/cubit/chat_cubit.dart
@@ -75,7 +75,7 @@ class ChatCubit extends Cubit<AiModelState> {
       final status = await voiceAssistant
           .speak(response.text ?? 'There is nothing to speak');
       print('status: $status');
-    } on GenerativeAIException catch (e) {
+    } on GenerativeAIException catch (_) {
       emit(state.copyWith(
           status:
               const AiResponseStatus.generatingAnswer('I cannot answer that')));
